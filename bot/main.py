66537742c61acba61e155f1734499e1d5f69f8bc
MathiasDPX <mathias@dupeux.net>
2025-11-26 20:37:35 +0100

    Add reactions

--- bot/main.py
+++ bot/main.py
@@ -3,10 +3,62 @@ from slack_bolt import App
 from datetime import datetime, timezone
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from database import *
+from os import getenv
 import traceback
+import json
 
 load_dotenv()
 app = App()
+bot_id = getenv("BOT_UID", "U09VC4NQXC6")
+
+reactions:dict = []
+with open("reactions.json", "r", encoding="utf-8") as f:
+    # load reactions or die
+    reactions = json.load(f)
+
+
+def remove_reactions(shortcut, client):
+    msg_id = shortcut["message"]["client_msg_id"]
+    ts = shortcut["message"]["ts"]
+    channel = shortcut["channel"]["id"]
+
+    result = client.conversations_history(
+        channel=channel, latest=ts, inclusive=True, limit=1
+    )
+
+    message = result.get("messages", [])[0]
+    reactions = message.get("reactions", [])
+
+    for reaction in reactions:
+        if bot_id not in reaction["users"]:
+            continue 
+        
+        client.reactions_remove(channel=channel, timestamp=ts, name=reaction["name"])
+
+
+def add_reactions(shortcut, client):
+    message = shortcut["message"]["text"]
+    channel = shortcut["channel"]["id"]
+    ts = shortcut["message"]["ts"]
+    
+    tags = []
+    for keyword, emoji in reactions.items():
+        if keyword not in message:
+            continue
+        
+        try:
+            client.reactions_add(
+                channel=channel,
+                timestamp=ts,
+                name=emoji
+            )
+        except:
+            pass
+        
+        tags.append(keyword)
+        
+    return tags
+
 
 
 @app.shortcut("unpost_message")
@@ -14,7 +66,6 @@ def handle_unpost(ack, shortcut, client):
     try:
         ack()
 
-        message = shortcut["message"]["text"]
         msg_id = shortcut["message"]["client_msg_id"]
         channel = shortcut["channel"]["id"]
         author = shortcut["message"]["user"]
@@ -28,15 +79,7 @@ def handle_unpost(ack, shortcut, client):
             )
             return
 
-        ts = shortcut["message"]["ts"]
-        ts = datetime.fromtimestamp(float(ts), tz=timezone.utc)
-
-        post = Post(
-            message_id=msg_id, author=author, message=message, timestamp=ts, tags=[]
-        )
-
-        post.save()
-
+        remove_reactions(shortcut, client)
         success = Post.delete_by_id(msg_id)
 
         if success:
@@ -45,7 +88,7 @@ def handle_unpost(ack, shortcut, client):
                 user=author,
                 text="You're post have been deleted!",
             )
-    except Exception as e:
+    except:
         client.chat_postEphemeral(
             channel=channel,
             user=author,
@@ -75,8 +118,9 @@ def handle_post(ack, shortcut, client):
         ts = shortcut["message"]["ts"]
         ts = datetime.fromtimestamp(float(ts), tz=timezone.utc)
 
+        tags = add_reactions(shortcut, client)
         post = Post(
-            message_id=msg_id, author=author, message=message, timestamp=ts, tags=[]
+            message_id=msg_id, author=author, message=message, timestamp=ts, tags=tags
         )
 
         success = post.save()
@@ -93,7 +137,7 @@ def handle_post(ack, shortcut, client):
                 user=author,
                 text="This has already been post :sadgua:",
             )
-    except Exception as e:
+    except:
         client.chat_postEphemeral(
             channel=channel,
             user=author,
